fa8100c7f23ea1ef16011b7e0491f792d1a9cc77
Richard <richardcarsongill@gmail.com>
2020-02-02 12:37:34 -0500

    set start

--- src/underbar.js
+++ src/underbar.js
@@ -270,10 +270,10 @@
   _.defaults = function(obj) {
     for (let i = 0; i < arguments.length; i++) {
       for (let key in arguments[i]) {
-        if (arguments[0].hasOwnProperty(key)) {
-          break;
-        }
+        if (!arguments[0].hasOwnProperty(key)) {
           arguments[0][key] = arguments[i][key]; 
+        }
+          
       }
     }
     return arguments[0]
